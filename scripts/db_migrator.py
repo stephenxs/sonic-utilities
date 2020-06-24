57a99c29085407d2eb47d0d7c707d5f9bb7a03b2
Stephen Sun <stephens@mellanox.com>
2020-06-24 03:10:16 +0000

    Prepare required tables in APPL_DB and STATE_DB for dynamic headroom including: BUFFER_POOL BUFFER_PROFILE
    Signed-off-by: Stephen Sun <stephens@mellanox.com>

--- scripts/db_migrator.py
+++ scripts/db_migrator.py
@@ -49,9 +49,15 @@ class DBMigrator():
 
         if namespace is None:
             self.configDB = ConfigDBConnector(**db_kwargs)
+            self.applDB = ConfigDBConnector(**db_kwargs)
+            self.stateDB = ConfigDBConnector(**db_kwargs)
         else:
             self.configDB = ConfigDBConnector(use_unix_socket_path=True, namespace=namespace, **db_kwargs)
+            self.applDB = ConfigDBConnector(use_unix_socket_path=True, namespace=namespace, **db_kwargs)
+            self.stateDB = ConfigDBConnector(use_unix_socket_path=True, namespace=namespace, **db_kwargs)
         self.configDB.db_connect('CONFIG_DB')
+        self.applDB.db_connect('APPL_DB')
+        self.stateDB.db_connect('STATE_DB')
 
     def migrate_pfc_wd_table(self):
         '''
@@ -249,6 +255,11 @@ class DBMigrator():
             log_error("Trying to get DEVICE_METADATA from DB but doesn't exist, skip migration")
             return False
 
+        if new_version == "version_1_0_5":
+            copy_to_appldb = True
+        else:
+            copy_to_appldb = False
+
         # SKUs that have single ingress buffer pool
         single_ingress_pool_skus = ['Mellanox-SN2700-C28D8', 'Mellanox-SN2700-D48C8', 'Mellanox-SN3800-D112C8']
         if not hwsku in single_ingress_pool_skus:
@@ -279,6 +290,11 @@ class DBMigrator():
  
         # Try to get related info from DB
         buffer_pool_conf = self.configDB.get_table('BUFFER_POOL')
+
+        # Copy buffer pools to APPL_DB
+        if copy_to_appldb:
+            for name, pool in buffer_pool_conf.iteritems():
+                self.applDB.set_entry('BUFFER_POOL', name, pool)
 
         # Get current buffer pool configuration, only migrate configuration which 
         # with default values, if it's not default, leave it as is.
@@ -340,10 +356,29 @@ class DBMigrator():
             log_info("Successfully migrate mlnx buffer pool size to the latest.")
         return True
 
-    def mlnx_migrate_buffer_profile(self, single_pool):
+
+    def mlnx_migrate_buffer_table_to_appl_db(self, entries, table_name, reference_field_name):
         """
-        This is to migrate BUFFER_PROFILE and BUFFER_PORT_INGRESS_PROFILE_LIST tables
+        tool function: copy tables from config db to appl db
+        """
+        for key, items in entries.iteritems():
+            # copy items to appl db
+            confdb_profile_ref = items[reference_field_name]
+            items[reference_field_name] = confdb_profile_ref.replace('|', ':')
+            if type(key) is tuple:
+                appl_db_key = ':'.join(key)
+            else:
+                appl_db_key = key
+            self.applDB.set_entry(table_name, appl_db_key, items)
+
+
+    def mlnx_migrate_buffer_profile(self, single_pool, warmreboot_to_dynamic_headroom):
+        """
+        from v_1_0_2 to v_1_0_3
+        to migrate BUFFER_PROFILE and BUFFER_PORT_INGRESS_PROFILE_LIST tables
         to single ingress pool mode for Mellanox SKU.
+        from v_1_0_3 to v_1_0_4
+        to migrate BUFFER_PROFILE to dynamic calculation mode
         """
         device_data = self.configDB.get_table('DEVICE_METADATA')
         if 'localhost' in device_data.keys():
@@ -373,14 +408,26 @@ class DBMigrator():
         buffer_port_ingress_profile_list_new = "[BUFFER_PROFILE|ingress_lossless_profile]"
 
         buffer_profile_conf = self.configDB.get_table('BUFFER_PROFILE')
+
+        copy_to_appl_only = False
         for name, profile in buffer_profile_old_configure.iteritems():
             if name in buffer_profile_conf.keys() and profile == buffer_profile_old_configure[name]:
                 continue
             # return if any default profile isn't in cofiguration
-            return True
+            if warmreboot_to_dynamic_headroom:
+                copy_to_appl_only = True
+            else:
+                return True
 
-        for name, profile in buffer_profile_new_configure.iteritems():
-            self.configDB.set_entry('BUFFER_PROFILE', name, profile)            
+        if copy_to_appl_only:
+            # don't migrate buffer profile configuration, just copy them to appl db
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_profile_conf, 'BUFFER_PROFILE', 'pool')
+            return True
+        else:
+            for name, profile in buffer_profile_new_configure.iteritems():
+                self.configDB.set_entry('BUFFER_PROFILE', name, profile)
+            buffer_profile_conf = self.configDB.get_table('BUFFER_PROFILE')
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_profile_conf, 'BUFFER_PROFILE', 'pool')
 
         if single_pool:
             buffer_port_ingress_profile_list_conf = self.configDB.get_table('BUFFER_PORT_INGRESS_PROFILE_LIST')
@@ -396,8 +443,15 @@ class DBMigrator():
 
         return True
 
-    def mlnx_migrate_buffer_dynamic_calculation(self):
+
+    def mlnx_migrate_buffer_dynamic_calculation(self, is_warmreboot):
         """
+        Migrate buffer tables to dynamic calculation mode
+        1. Remove the profiles generated according to pg_headroom_lookup.ini
+           unless there alpha isn't the default value (0 for mellanox)
+        2. Insert tables required for dynamic buffer calculation
+        3. For lossless dynamic PGs, remove the explicit referencing buffer profiles
+        4. Copy all other tables to the application db
         """
         # Migrate BUFFER_PROFILEs, removing dynamically generated profiles
         dynamic_profile = self.configDB.get_table('BUFFER_PROFILE')
@@ -427,6 +481,7 @@ class DBMigrator():
 
         cable_lengths = all_cable_lengths[all_cable_lengths.keys()[0]]
         for name, profile in buffer_pgs.iteritems():
+            # do the db migration
             port, pg = name
             if pg != '3-4':
                 continue
@@ -443,6 +498,16 @@ class DBMigrator():
                     self.configDB.set_entry('BUFFER_PG', name, {'NULL': 'NULL'})
             except:
                 continue
+
+        # copy BUFFER_QUEUE, BUFFER_PORT_INGRESS_PROFILE_LIST and BUFFER_PORT_EGRESS_PROFILE_LIST to appl db
+        if is_warmreboot:
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_pgs, 'BUFFER_PG', 'profile')
+            buffer_queues = self.configDB.get_table('BUFFER_QUEUE')
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_queues, 'BUFFER_QUEUE', 'profile')
+            buffer_port_ingress_profile_list = self.configDB.get_table('BUFFER_PORT_INGRESS_PROFILE_LIST')
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_port_ingress_profile_list, 'BUFFER_PORT_INGRESS_PROFILE_LIST', 'profile_list')
+            buffer_port_egress_profile_list = self.configDB.get_table('BUFFER_PORT_EGRESS_PROFILE_LIST')
+            self.mlnx_migrate_buffer_table_to_appl_db(buffer_port_egress_profile_list, 'BUFFER_PORT_EGRESS_PROFILE_LIST', 'profile_list')
 
         return True
 
@@ -486,6 +551,7 @@ class DBMigrator():
         # Check ASIC type, if Mellanox platform then need DB migration
         version_info = sonic_device_util.get_sonic_version_info()
         if version_info['asic_type'] == "mellanox":
+            # This is to migrate the buffer size according to the sdk update
             if self.mlnx_migrate_buffer_pool_size('version_1_0_2', 'version_1_0_3'):
                 self.set_version('version_1_0_3')
         else:
@@ -501,8 +567,9 @@ class DBMigrator():
         # Check ASIC type, if Mellanox platform then need DB migration
         version_info = sonic_device_util.get_sonic_version_info()
         if version_info['asic_type'] == "mellanox":
+            # This is to migrate the buffer settings to single ingress pool mode
             if self.mlnx_migrate_buffer_pool_size('version_1_0_3', 'version_1_0_4') \
-               and self.mlnx_migrate_buffer_profile(True):
+               and self.mlnx_migrate_buffer_profile(True, False):
                 self.set_version('version_1_0_4')
         else:
             self.set_version('version_1_0_4')
@@ -517,9 +584,17 @@ class DBMigrator():
 
         version_info = sonic_device_util.get_sonic_version_info()
         if version_info['asic_type'] == "mellanox":
+            # This is to migrate to dynamic buffer calculation
+            warmreboot_state = self.stateDB.get_entry('WARM_RESTART_ENABLE_TABLE', 'system')
+            if 'enable' in warmreboot_state.keys():
+                is_warmreboot = warmreboot_state['enable'] == 'true'
+            else:
+                is_warmreboot = False
+            if is_warmreboot:
+                self.stateDB.set_entry('WARM_RESTART_TABLE', 'buffermgrd', {'restore_count': '0'})
             if self.mlnx_migrate_buffer_pool_size('version_1_0_4', 'version_1_0_5') \
-               and self.mlnx_migrate_buffer_profile(False) \
-               and self.mlnx_migrate_buffer_dynamic_calculation():
+               and self.mlnx_migrate_buffer_profile(False, is_warmreboot) \
+               and self.mlnx_migrate_buffer_dynamic_calculation(is_warmreboot):
                 self.set_version('version_1_0_5')
         else:
             self.set_version('version_1_0_5')
